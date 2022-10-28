c9817173f9bc8b14c361d60f453847bdcc00818c
“carlos <cordovamanuel22@icloud.com>
2022-10-27 17:09:28 -0700

    html updated on buttons

--- main.js
+++ main.js
@@ -7,21 +7,18 @@ const upcId = document.querySelector('#upc-id');   // calls upc input
 const seller = document.querySelector('#seller'); // calls  sellers input
 const price = document.querySelector('#price'); // calls price input
 const addProduct = document.querySelector('#bnt-summit');// add a product to my inventory
+const editProduct = document.querySelector('#bnt-edits'); // edits product to inventory
 const tDataContainer = document.querySelector('tbody'); // table row tbody
-
-
 
 //---===EVENT LISTENER ===--- 
 addProduct.addEventListener('click', send); // adds a product to produts/inventory  array
-document.addEventListener("DOMContentLoaded",prints);// prins elements to when we open page
-
-
+document.addEventListener("DOMContentLoaded", prints);// prins elements to when we open page
 
 // ---===FUNTIONS ===---
 
 // sends elements to local stogage and prints them
 function send() {
-  
+
 
   // does not let send form if not fill complete
   if (productName.value == '' | upcId.value == '' | seller.value == '' | price.value == '') {
@@ -29,9 +26,6 @@ function send() {
     return;
 
   }
-
-
-
 
   // creates a object for inventory 
   objInventory = {
@@ -43,9 +37,9 @@ function send() {
 
   }
   //--- LOCAL STORAGE ==---
-// variable to set localstogare
+  // variable to set localstogare
   let inventory = JSON.parse(localStorage.getItem('products'));
-// if for localstogage
+  // if for localstogage
   if (inventory === null) {
     inventory = [];
     inventory.push(objInventory);
@@ -58,7 +52,7 @@ function send() {
   prints(); // prints html elemenets to tDatacontainer
   // prevent form from submitting
   e.preventDefault();
-  
+
   // clear input form  after an input 
   productName.value = '';
   upcId.value = '';
@@ -66,7 +60,6 @@ function send() {
   price.value = '';
 
 }
-
 
 // create table row and table data using innerHTML.
 function prints() {
@@ -77,9 +70,9 @@ function prints() {
     tDataContainer.innerHTML = "";
 
     inventory.forEach((element) => {
-      tDataContainer.innerHTML = 
-      tDataContainer.innerHTML + 
-      `
+      tDataContainer.innerHTML =
+        tDataContainer.innerHTML +
+        `
  <tr class='trContainer' key = ${element.id}> 
    <td>${element.name}</td>
    <td>${element.upc}</td>
@@ -94,27 +87,41 @@ function prints() {
   }
 }
 
-
 // DELETE FUNTION 
-function onDeleteRow(e){
+function onDeleteRow(e) {
   //console.log(e.path[2].childNodes[13].innerHTML)
   // console.log(e.path[2].getAttribute('key'));
-   let findTr = e.path[2].getAttribute('key'); // to  find element id or key 
+  let findTr = e.path[2].getAttribute('key'); // to  find element id or key 
 
   let inventory = JSON.parse(localStorage.getItem('products'));
   let index = inventory.findIndex((element) => element.id == findTr);
 
-  inventory.splice(inventory,1); // delete element from array
+  inventory.splice(inventory, 1); // delete element from array
 
   localStorage.setItem('products', JSON.stringify(inventory)); // updated DOM OR localstorage
 
   prints()
 
-  form.reset();
+   // clear input form  after an input 
+   productName.value = '';
+   upcId.value = '';
+   seller.value = '';
+   price.value = '';
 
 }
 // update function or edit function
-function update(e){
+function update(e) {
+
+  let findTr = e.path[2].getAttribute('key'); // to  find element id or key 
+  let inventory = JSON.parse(localStorage.getItem('products')); // to bring DOM
+  let index = inventory.findIndex((element) => element.id == findTr); // TO FIND INDEX
+
+  // to bring data into inputs 
+  productName.value = inventory[index].name;
+  upcId.value = inventory[index].upc;
+  seller.value = inventory[index].sellers;
+  price.value = inventory[index].cost;
+
 
 }
 
